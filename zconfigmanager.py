56c320907137773753a3959b76948e17c76e4996
fieldOfView <aldo@fieldofview.com>
2015-01-05 21:10:01 +0100

    Refactor into a reusable class

--- zconfigmanager.py
+++ zconfigmanager.py
@@ -7,53 +7,72 @@ import socket
 import time
 import json
 
-# maintain a list of peer names
-peers_names = {}
 
-def on_peer_enter(peer, name, *args, **kwargs):
-    peers_names[peer.hex] = name
+class ZConfigManagerNode(ZOCP):
+    def __init__(self, nodename=""):
+        self.peers_names = {}
+
+        super().__init__()
+
+        self.set_name(nodename)
+        self.start()
+
+
+    def stop(self):
+        print("Closing ZOCP node...")
+        super().stop()
+
+
+    def discover(self, duration = 0.5):
+        print("Discovering ZOCP network...")
+        start = time.time()
+        while (time.time() - start) < duration:
+            z.run_once(0)
+
+
+    def build_network_tree(self):
+        # create network description
+        peers = {}
+        for peer, capabilities in z.peers_capabilities.items():
+            # store node name
+            peer_name = self.peers_names[peer.hex]
+            print("Adding node '%s' (%s)..." % (peer_name, peer.hex))
+            capabilities["_name"] = peer_name
+
+            # add node names to subscribers
+            for c in capabilities:
+                if "subscribers" in capabilities[c]:
+                    subscribers = capabilities[c]["subscribers"]
+                    for s in range(0, len(subscribers)):
+                        capabilities[c]["subscribers"][s].append( 
+                                self.peers_names[subscribers[s][0]])
+
+            peers[peer.hex] = capabilities
+
+        return peers
+
+
+    def write(self, filename, tree):
+        print("Writing to file...")
+        f = open(filename, "w")
+        f.write(json.dumps(network_tree, indent=4, sort_keys=True))
+        f.close()
+
+
+    def on_peer_enter(self, peer, name, *args, **kwargs):
+        # maintain a list of peer names
+        self.peers_names[peer.hex] = name
 
 
 if __name__ == '__main__':
-    zl = logging.getLogger("zocp")
-    zl.setLevel(logging.INFO)
-
     # setup ZOCP node, and run it for some time to discover
     # the current network
-    z = ZOCP()
-    z.on_peer_enter = on_peer_enter
-    z.set_name("ConfigManager@%s" % socket.gethostname())
-    z.start()
-    print("Discovering ZOCP network...")
-    start = time.time()
-    while (time.time() - start) < 0.5:
-        z.run_once(0)
-
-    # create network description
-    peers = {}
-    for peer, capabilities in z.peers_capabilities.items():
-        # store node name
-        peer_name = peers_names[peer.hex]
-        print("Adding node '%s' (%s)..." % (peer_name, peer.hex))
-        capabilities["_name"] = peer_name
-
-        # add node names to subscribers
-        for c in capabilities:
-            if "subscribers" in capabilities[c]:
-                subscribers = capabilities[c]["subscribers"]
-                for s in range(0, len(subscribers)):
-                    capabilities[c]["subscribers"][s].append( peers_names[subscribers[s][0]] )
-
-        peers[peer.hex] = capabilities
+    z = ZConfigManagerNode("ConfigManager@%s" % socket.gethostname())
+    z.discover(0.5)
+    network_tree = z.build_network_tree()
 
     # write network description to file
-    print("Writing to file...")
-    f = open("network.json", "w")
-    f.write(json.dumps(peers, indent=4, sort_keys=True))
-    f.close()
+    z.write("network.json", network_tree)
 
     # shut down ZOCP node
-    print("Closing ZOCP node...")
     z.stop()
-
-
